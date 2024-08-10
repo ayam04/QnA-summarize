42803e637988728c1f1e42314580875191a893f9
ayam04 <ayamullahkhan04@gmail.com>
2024-08-10 15:03:36 +0400

    upd

--- main.py
+++ main.py
@@ -56,6 +56,64 @@ def time_analysis(path):
     output_file_path = "Screening_Time_Analysis.xlsx"
     result_df.to_excel(output_file_path)
 
+def avergae_time_analysis(path):
+    df = pd.read_excel(path)
+    
+    time_classes = [
+        (0, 2, '0-2 hrs'),
+        (2, 4, '2-4 hrs'),
+        (4, 8, '4-8 hrs'),
+        (8, 12, '8-12 hrs'),
+        (12, 24, '12-24 hrs'),
+        (24, 48, '24-48 hrs'),
+        (48, float('inf'), '>48 hrs')
+    ]
+    
+    def convert_time_to_hours(time_str):
+        if isinstance(time_str, str):
+            if 'Pending' in time_str:
+                return 'Screen Pending'
+            if 'Less Than Minute' in time_str:
+                return 1 / 60
+            if 'Minute' in time_str:
+                minutes = int(re.search(r'(\d+) Minute', time_str).group(1))
+                return minutes / 60
+            if 'Hour' in time_str:
+                hours = int(re.search(r'(\d+) Hour', time_str).group(1))
+                return hours
+            if 'Day' in time_str:
+                days = int(re.search(r'(\d+) Day', time_str).group(1))
+                return days * 24
+        return None
+    
+    df['Time in Hours'] = df['Screening Completed In'].apply(convert_time_to_hours)
+    
+    def classify_time(hours):
+        if hours == 'Screen Pending':
+            return hours
+        for lower, upper, label in time_classes:
+            if lower <= hours < upper:
+                return label
+        return '>48 hrs'
+    
+    df['Time Class'] = df['Time in Hours'].apply(classify_time)
+    df['Under 5 Minutes'] = df['Time in Hours'].apply(lambda x: x < (5 / 60) if isinstance(x, (int, float)) else 0)
+    time_class_counts = df['Time Class'].value_counts()
+    total_counts = len(df)
+    time_class_percentage = (time_class_counts / total_counts) * 100
+    
+    under_5_minutes_percentage = df['Under 5 Minutes'].mean() * 100
+    time_class_df = time_class_percentage.reset_index()
+    time_class_df.columns = ['Time Class', 'Percentage']
+    
+    under_5_minutes_df = pd.DataFrame({
+        'Time Class': ['Under 5 Minutes'],
+        'Percentage': [under_5_minutes_percentage]
+    })
+    
+    combined_df = pd.concat([time_class_df, under_5_minutes_df], ignore_index=True)
+    combined_df.to_excel("Avg_Screening_Time_Analysis.xlsx", sheet_name='Time Analysis', index=False)
+
 def answer_analysis(path):
     df = pd.read_excel(path)
     result = df.groupby(['Job Title', 'Question', 'Correct Answer']).size().unstack(fill_value=0)
@@ -67,5 +125,6 @@ def answer_analysis(path):
     # print(final_result)
 
 
-answer_analysis('Tests/test.xlsx')
-time_analysis("Tests/Time Analysis Test.xlsx")
+# answer_analysis('Tests/test.xlsx')
+# time_analysis("Tests/Time Analysis Test.xlsx")
+avergae_time_analysis("Tests/Time Analysis Test.xlsx")
